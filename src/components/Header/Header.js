2e6ff2feafe8cf2827c497773981523a4caa53db
fajariadi-id <fajariadi.id@gmail.com>
2021-02-26 14:18:21 +0700

    add: saran/masukan

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,6 +20,25 @@ const Header = () => {
         color='#3730A3'
         onClick={() => dispatch(openModal())}
       />
+
+      <Pesan>
+        Saran/masukan untuk perbaikan{' '}
+        <a href='https://kompas.id'>https://kompas.id</a>
+      </Pesan>
+      <TextContainer>
+        <p>
+          Sebagai website penyedia berita, <em>Kompas.id</em> terlihat rapi dan
+          baik. Tidak banyak iklan yang mengganggu dan semacamnya.
+        </p>
+        <p>
+          Sebagai website penyedia berita juga <em>Kompas.id</em> terbilang
+          sangat informatif. Tentu sebagai penyedia berita ingin informasinya
+          dapat dinikmati setiap kalangan. Untuk menggapai setiap kalangan
+          tersebut, mungkin dengan memberikan tampilan yang sedikit berbeda
+          mampu menarik minat pembaca serta nyaman untuk selalu mengakses berita
+          di <em>Kompas.id</em>.
+        </p>
+      </TextContainer>
     </HeaderContainer>
   );
 };
@@ -32,6 +51,22 @@ const HeaderContainer = styled.header`
     margin-bottom: 2rem;
     font-size: 24px;
   }
+`;
+
+const Pesan = styled.span`
+  display: block;
+  margin: 20px auto 0;
+  background-color: #00599a;
+  color: white;
+  width: fit-content;
+  padding: 5px 10px;
+  border-radius: 5px;
+`;
+
+const TextContainer = styled.div`
+  width: 100%;
+  max-width: 700px;
+  margin: 10px auto 0;
 `;
 
 export default Header;
