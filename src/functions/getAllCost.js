950cf198cd152bfb2627b7a695476b03acd40432
fajariadi-id <fajariadi.id@gmail.com>
2021-03-01 17:38:07 +0700

    update: documentation

--- src/functions/getAllCost.js
+++ src/functions/getAllCost.js
@@ -1,8 +1,10 @@
 import { getItemCreated } from '../functions';
 
 const getAllCost = (receipt) => {
+  // "2021-03-01 17:00:27" -> ["2021-03-01", "17:00:27"] -> ["2021", "03", "01"] -> "03"
   const month = getItemCreated().split(' ')[0].split('-')[1];
   const getRecentlyMonth = receipt.filter(
+    // "2021-03-01 17:00:27" -> ["2021-03-01", "17:00:27"] -> ["2021", "03", "01"] -> "03" === "03"
     (item) => item.created_at.split(' ')[0].split('-')[1] === month
   );
 
--- src/functions/headerDateFormat.js
+++ src/functions/headerDateFormat.js
@@ -1,11 +1,14 @@
 import { getItemCreated } from '../functions';
 
 const headerDateFormat = (months) => {
+  // "2021-03-01 17:00:27" -> ["2021-03-01", "17:00:27"] -> ["2021", "03", "01"] -> "03"
   const month = getItemCreated().split(' ')[0].split('-')[1];
   const getMonthNumber = month[0] === '0' ? month.slice(-1) : month;
 
+  // "2021-03-01 17:00:27" -> ["2021-03-01", "17:00:27"] -> ["2021", "03", "01"] -> "2021"
   const getYear = getItemCreated().split(' ')[0].split('-')[0];
 
+  // months[2] -> "Maret"
   return `${months[getMonthNumber - 1]} ${getYear}`;
 };
 
